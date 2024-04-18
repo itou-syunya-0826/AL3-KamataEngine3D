074920e83466c5fb86a029b04d0e5f1e04417980
itou-syunya-0826 <146901442+itou-syunya-0826@users.noreply.github.com>
2024-04-18 13:06:41 +0900

    ImGuiまでやったのをいろいろ追加した

--- DirectXGame/scene/GameScene.cpp
+++ DirectXGame/scene/GameScene.cpp
@@ -1,19 +1,59 @@
 #include "GameScene.h"
 #include "TextureManager.h"
 #include <cassert>
+#include "ImGuiManager.h"
 
 GameScene::GameScene() {}
 
-GameScene::~GameScene() {}
+GameScene::~GameScene() { 
+	delete sprite_;
+	delete model_;
+}
 
 void GameScene::Initialize() {
 
 	dxCommon_ = DirectXCommon::GetInstance();
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
+
+	textureHandle_ = TextureManager::Load("sample.png");
+	sprite_ = Sprite::Create(textureHandle_, {100, 50});
+
+	model_ = Model::Create();
+
+	worldTransform_.Initialize();
+	viewProjection_.Initialize();
+
+	soundDataHandle_ = audio_->LoadWave("mokugyo.wav");//fanfare.wav
+	/*audio_->PlayWave(soundDataHandle_);*/
+	voiceHandle_ = audio_->PlayWave(soundDataHandle_, true);//PlayWaveは二回書いちゃダメ（二個再生するから停止しても意味ない）
 }
 
-void GameScene::Update() {}
+void GameScene::Update() {
+	Vector2 position = sprite_->GetPosition();
+
+	position.x += 2.0f;
+	position.y += 1.0f;
+
+	sprite_->SetPosition(position);
+	//スペースキー押した瞬間
+	if (input_->TriggerKey(DIK_SPACE)) {
+		if (audio_->IsPlaying(voiceHandle_)) {
+			audio_->StopWave(voiceHandle_);
+		} 
+		else 
+		{
+			voiceHandle_ = audio_->PlayWave(soundDataHandle_,true);
+		}
+	}
+
+	ImGui::Begin("Debug1");
+	ImGui::Text("Kamata Tarou %d.%d.%d", 2050, 12, 31);
+	ImGui::InputFloat3("Inputfloat3", inputFloat3);
+	ImGui::SliderFloat3("SliderFloat3", inputFloat3, 0.0f, 1.0f);
+	ImGui::ShowDemoWindow();
+	ImGui::End();
+}
 
 void GameScene::Draw() {
 
@@ -27,6 +67,7 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに背景スプライトの描画処理を追加できる
 	/// </summary>
+	
 
 	// スプライト描画後処理
 	Sprite::PostDraw();
@@ -41,6 +82,7 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
+	model_->Draw(worldTransform_, viewProjection_, textureHandle_);
 
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
@@ -53,6 +95,7 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに前景スプライトの描画処理を追加できる
 	/// </summary>
+	sprite_->Draw();
 
 	// スプライト描画後処理
 	Sprite::PostDraw();
