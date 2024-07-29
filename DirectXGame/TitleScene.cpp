50113a0c3bfb976d50f11d926778f43449af65b5
itou-syunya-0826 <146901442+itou-syunya-0826@users.noreply.github.com>
2024-07-29 17:34:28 +0900

    タイトルを表示させた

--- DirectXGame/TitleScene.cpp
+++ DirectXGame/TitleScene.cpp
@@ -1,13 +1,71 @@
 #include "TitleScene.h"
+#include "imgui.h"
 
-void TitleScene::Initialize() {}
+TitleScene::TitleScene() {}
+
+TitleScene::~TitleScene() {}
+
+void TitleScene::Initialize() {
+
+	dxCommon_ = DirectXCommon::GetInstance();
+	input_ = Input::GetInstance();
+	audio_ = Audio::GetInstance();
+
+	model_ = Model::CreateFromOBJ("Title", true);
+	viewProjection_.Initialize();
+	worldTransform_.Initialize();
+	worldTransform_.scale_ = Vector3(10.0f, 10.0f, 1.0f);
+}
 
 void TitleScene::Update() {
 
 	if (Input::GetInstance()->PushKey(DIK_SPACE)) {
 		finished_ = true;
 	}
-
+	worldTransform_.UpdateMatrix();
 }
 
-void TitleScene::Draw() {}
+	void TitleScene::Draw() {
+		// コマンドリストの取得
+		ID3D12GraphicsCommandList* commandList = dxCommon_->GetCommandList();
+
+#pragma region 背景スプライト描画
+		// 背景スプライト描画前処理
+		Sprite::PreDraw(commandList);
+
+		/// <summary>
+		/// ここに背景スプライトの描画処理を追加できる
+		/// </summary>
+
+		// スプライト描画後処理
+		Sprite::PostDraw();
+		// 深度バッファクリア
+		dxCommon_->ClearDepthBuffer();
+#pragma endregion
+
+#pragma region 3Dオブジェクト描画
+		// 3Dオブジェクト描画前処理
+		Model::PreDraw(commandList);
+
+		/// <summary>
+		/// ここに3Dオブジェクトの描画処理を追加できる
+		/// </summary>
+		model_->Draw(worldTransform_, viewProjection_);
+
+		// 3Dオブジェクト描画後処理
+		Model::PostDraw();
+#pragma endregion
+
+#pragma region 前景スプライト描画
+		// 前景スプライト描画前処理
+		Sprite::PreDraw(commandList);
+
+		/// <summary>
+		/// ここに前景スプライトの描画処理を追加できる
+		/// </summary>
+
+		// スプライト描画後処理
+		Sprite::PostDraw();
+
+#pragma endregion
+	}
--- DirectXGame/TitleScene.h
+++ DirectXGame/TitleScene.h
@@ -1,11 +1,19 @@
 #pragma once
-#include<Input.h>
+#include "Audio.h"
+#include "DirectXCommon.h"
+#include "Input.h"
+#include "Model.h"
+#include "Sprite.h"
+#include "ViewProjection.h"
+#include "WorldTransform.h"
 
 /// <summary>
 /// タイトルシーン
 /// </summary>
 class TitleScene {
 public:
+	TitleScene();
+	~TitleScene();
 
 	void Initialize();
 	void Update();
@@ -15,7 +23,25 @@ public:
 	bool IsFinished() const { return finished_; }
 
 private:
+	DirectXCommon* dxCommon_ = nullptr;
+	Input* input_ = nullptr;
+	Audio* audio_ = nullptr;
+
+	/// <summary>
+	/// ワールド変換データ
+	/// </summary>
+	WorldTransform worldTransform_;
+	/// <summary>
+	/// モデル
+	/// </summary>
+	Model* model_ = nullptr;
+	/// <summary>
+	/// ビュープロジェクション
+	/// </summary>
+	ViewProjection viewProjection_;
+
+	Model* titlemodel_;
+
 	// 終了フラグ
 	bool finished_ = false;
-	
 };
